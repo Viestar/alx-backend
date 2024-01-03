bcceaedbb5475bac632e347e3a728308bfe51481
Viestar <m.silverbuckson@gmail.com>
2024-01-04 00:09:21 +0300

    100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -20,26 +20,26 @@ class LFUCache(BaseCaching):
         recently used item.
         """
 
-        ins_pos = 0
+        ins_position = 0
         max_positions = []
-        mru_freq = 0
-        mru_pos = 0
-        for i, key_freq in enumerate(self.keys_freq):
-            if key_freq[0] == mru_key:
-                mru_freq = key_freq[1] + 1
-                mru_pos = i
+        mru_frequency = 0
+        mru_position = 0
+        for i, key_frequency in enumerate(self.keys_freq):
+            if key_frequency[0] == mru_key:
+                mru_frequency = key_frequency[1] + 1
+                mru_position = i
                 break
             elif len(max_positions) == 0:
                 max_positions.append(i)
-            elif key_freq[1] < self.keys_freq[max_positions[-1]][1]:
+            elif key_frequency[1] < self.keys_freq[max_positions[-1]][1]:
                 max_positions.append(i)
         max_positions.reverse()
         for pos in max_positions:
-            if self.keys_freq[pos][1] > mru_freq:
+            if self.keys_freq[pos][1] > mru_frequency:
                 break
-            ins_pos = pos
-        self.keys_freq.pop(mru_pos)
-        self.keys_freq.insert(ins_pos, [mru_key, mru_freq])
+            ins_position = pos
+        self.keys_freq.pop(mru_position)
+        self.keys_freq.insert(ins_position, [mru_key, mru_frequency])
 
     def put(self, key, item):
         """Adds an item in the cache.
@@ -54,8 +54,8 @@ class LFUCache(BaseCaching):
                 print("DISCARD:", lfu_key)
             self.cache_data[key] = item
             ins_index = len(self.keys_freq)
-            for i, key_freq in enumerate(self.keys_freq):
-                if key_freq[1] == 0:
+            for i, key_frequency in enumerate(self.keys_freq):
+                if key_frequency[1] == 0:
                     ins_index = i
                     break
             self.keys_freq.insert(ins_index, [key, 0])
