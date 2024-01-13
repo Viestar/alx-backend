61e2a8209861ce86eadd925e3489b4f10b3342bb
Viestar <m.silverbuckson@gmail.com>
2024-01-13 18:41:51 +0300

    2-hypermedia_pagination.py

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -52,8 +52,18 @@ def get_page(self, page: int = 1, page_size: int = 10) -> List[list]:
         return []
 
 
-def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[int, int, List[list], int, int, int]:
+def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, any]:
     """ Returns a dictionary with the pagination details """
     data = self.dataset()
-    index = index_range(page, page_size)
-    data = get_page(page, page_size)
+    total_pages = math.ceil(len(data) / page_size)
+    prev_page = page + 1 if page <= total_pages else None
+    next_page = page - 1 if page > 1 else None
+
+    return {
+        "page_size": len(self.get_page(page, page_size)),
+        "page": page,
+        "data": self.get_page(page, page_size),
+        "next_page": next_page,
+        "prev_page": prev_page,
+        "total_pages": total_pages
+    }
